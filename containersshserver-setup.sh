6896c42c3b126de181abf11e4bef5518f72d56bf
rcantabrana <rcantabrana@A0112110.dc.aragon.es>
2020-07-14 10:12:41 +0200

    comment user password info

--- containersshserver-setup.sh
+++ containersshserver-setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 #echo user passed $withUser - password $withPassword
+
 export ENV_USER=${withUser:-sae}
 export ENV_PASSWORD=${withPassword:-$ENV_USER}
 export userPath="/home/$ENV_USER"
+
 echo $userPath -- $username
 if [ ! -d "$userPath" ]; then
     echo "$userPath not exists";
