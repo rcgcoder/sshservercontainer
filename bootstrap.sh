34d9d7d4dbe50e2aa3062be566f3a1ae5fa8c791
rcantabrana <rcantabrana@A0112110.dc.aragon.es>
2020-07-15 09:13:27 +0200

    remove logs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@
 counter=$(ps -ef | grep sshd | wc -l)
 while [ $counter -gt 1 ]
 do
-    echo "servidor sshd activo";
+#    echo "servidor sshd activo";
     sleep 1m;
 	counter=$(ps -ef | grep /usr/sbin/sshd | wc -l)
 done
-
+echo "sshd is dead... kill container"
